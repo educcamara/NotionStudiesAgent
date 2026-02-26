532f9aff53a45c233d11aaa89315454349ab28e1
Eduardo Cordeiro da Camara <eduardo.camara@icomp.ufam.edu.br>
2026-02-26 15:56:05 -0400

    feat: implement Parallel Agent to run recipe file creator and writer in parallel

---
+++ notion_studies/agents/parallel_agent.py
@@ -0,0 +1,15 @@
+from google.adk.agents import ParallelAgent
+
+from notion_studies.agents.recipe_file_creator import recipe_file_creator
+from notion_studies.agents.recipe_researcher import recipe_researcher
+
+parallel_agent = ParallelAgent(
+    name="Parallel Agent",
+    description="""
+    Agent that runs both the recipe file creator (to create the recipe file in Notion) and the recipe writer (to fill the recipe content in the created file) in parallel.
+    """,
+    sub_agents=[
+        recipe_researcher,
+        recipe_file_creator,
+    ]
+)
