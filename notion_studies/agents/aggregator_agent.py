3d0ebc8bf47bb92318d2515fc5c54a37a97e5cc7
Eduardo Cordeiro da Camara <eduardo.camara@icomp.ufam.edu.br>
2026-02-26 16:12:44 -0400

    feat: add Aggregator Agent to aggregate outputs from recipe researcher and file creator

---
+++ notion_studies/agents/aggregator_agent.py
@@ -0,0 +1,33 @@
+from google.adk import Agent
+from google.adk.tools import AgentTool
+
+from notion_studies.agents.recipe_writer import recipe_writer
+
+aggregator_agent = Agent(
+    name="Aggregator Agent",
+    model="gemini-3-flash-preview",
+    description="""
+    Gets the results from recipe_researcher and recipe_file_creator, and aggregates them to provide a final status of the overall process.
+    """,
+    instruction="""
+The previous steps have produced the following outputs:
+- Recipe Researcher Output: {recipe_researcher}
+- Recipe File Creator Output: {recipe_file_creator}
+
+Your task is to:
+1. Save the recipe content with the recipe_writer tool in the created Notion file if the recipe was not found and the file was created.
+2. If it all went as expected, return:
+{
+    "status": "success",
+    "notion_file_path": "<the path of the created recipe file in Notion>",
+    "recipe_content": "<the recipe content that was saved in Notion>"
+}
+
+if there was an error in any of the previous steps, return:
+{
+    "status": "error",
+    "description": "<brief description of what went wrong>"
+}
+    """,
+    tools=[AgentTool(recipe_writer)]
+)
